1521499b46712c73b5927da0a592cb7dc4942e22
ohperes <peresvini2@gmail.com>
2022-11-22 16:15:46 -0300

    Import styled-components and set theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/theme'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
