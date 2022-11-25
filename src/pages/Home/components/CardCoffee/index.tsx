f0f2a404dcf97dd4f01df4e0b00aa5ddc12f5c78
ohperes <peresvini2@gmail.com>
2022-11-24 23:23:11 -0300

    Create CardCoffee component ☕

---
+++ src/pages/Home/components/CardCoffee/index.tsx
@@ -0,0 +1,52 @@
+import { ShoppingCart } from 'phosphor-react'
+import { useTheme } from 'styled-components'
+import { Counter } from '../../../../components/Counter'
+import { DefaultTheme } from '../../../../styles/themes/default'
+import { Text, Title } from '../../../../styles/themes/textRule'
+import { CardContainer, CoffeeTag, Description, Price } from './styles'
+
+interface Coffee {
+  id: number
+  name: string
+  description: string
+  image: string
+  tags: string[]
+  price: number
+}
+
+interface CardCoffeeProps {
+  coffee: Coffee
+}
+
+export function CardCoffee({ coffee }: CardCoffeeProps) {
+  const theme = useTheme() as DefaultTheme
+  return (
+    <CardContainer>
+      <img
+        src={`../../../../src/assets/coffeeCardImage/${coffee.image}`}
+        alt=""
+      />
+      <div className="tagList">
+        {coffee.tags.map((tag) => {
+          return <CoffeeTag key={`${coffee.id}-${tag}`}>{tag}</CoffeeTag>
+        })}
+      </div>
+      <></>
+      <Title size={'s'} weight={'bold'} color={theme.palette.base.subtitle}>
+        {coffee.name}
+      </Title>
+      <Description>{coffee.description}</Description>
+      <footer>
+        <Text size={'s'}>R$</Text>
+        <Price>{coffee.price.toFixed(2).toString().replace('.', ',')}</Price>
+
+        <div className="actions">
+          <Counter />
+          <div className="cart">
+            <ShoppingCart size={22} weight="fill" color={theme.palette.white} />
+          </div>
+        </div>
+      </footer>
+    </CardContainer>
+  )
+}
---
+++ src/pages/Home/components/CardCoffee/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+import { Text, Title } from '../../../../styles/themes/textRule'
+
+export const CardContainer = styled.div`
+  img {
+    margin-top: -1.25rem;
+  }
+
+  padding: 0 1.5625rem 1.25rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+
+  width: 16rem;
+
+  border-radius: 6px 36px 6px 36px;
+
+  background: ${(props) => props.theme.palette.base.card};
+
+  .tagList {
+    margin: 0.75rem 0 1rem 0;
+    display: flex;
+    flex-direction: row;
+    gap: 0.25rem;
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+
+    .actions {
+      display: flex;
+      gap: 0.5rem;
+      margin-left: 1.4375rem;
+
+      .cart {
+        display: flex;
+        align-items: center;
+        padding: 8px;
+        border-radius: 8px;
+        background: ${(props) => props.theme.palette.purple.dark};
+        position: relative;
+      }
+    }
+  }
+`
+export const CoffeeTag = styled.p`
+  font-weight: bold;
+  font-size: 10px;
+  color: ${(props) => props.theme.palette.yellow.dark};
+
+  padding: 0.25rem 0.5rem;
+  border-radius: 100px;
+  background: ${(props) => props.theme.palette.yellow.light};
+`
+export const Description = styled(Text).attrs({
+  size: 's',
+  color: 'label',
+})`
+  margin: 0.5rem 1.25rem;
+`
+export const Price = styled(Title).attrs({
+  size: 'm',
+  color: 'text',
+})`
+  margin-left: 2px;
+`
