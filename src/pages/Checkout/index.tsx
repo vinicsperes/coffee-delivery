e30044cfd678894274f93c372b287de51df1526e
ohperes <peresvini2@gmail.com>
2022-11-28 21:37:56 -0300

    Create Checkout page 🛒

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,158 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+  Trash,
+} from 'phosphor-react'
+import { useTheme } from 'styled-components'
+import { InputContent } from '../../components/FormInput/styles'
+import { DefaultTheme } from '../../styles/themes/default'
+import { Title, Text } from '../../styles/themes/textRule'
+import {
+  CardContent,
+  CheckoutComponent,
+  CoffeeSelected,
+  ConfirmOrderButton,
+  DeleteButton,
+  InfoSection,
+  SelectPaymentMethod,
+} from './styles'
+import { coffees } from '../../data/coffeList'
+import { Counter } from '../../components/Counter'
+
+export function Checkout() {
+  const theme = useTheme() as DefaultTheme
+
+  return (
+    <CheckoutComponent>
+      <div>
+        <Title size="xs" color="subtitle">
+          Complete seu pedido
+        </Title>
+        <CardContent>
+          <InfoSection>
+            <div>
+              <MapPinLine size={22} color={theme.palette.yellow.dark} />
+            </div>
+            <div>
+              <Text size="m" color="subtitle">
+                Endereço de Entrega
+              </Text>
+              <Text size="s" color="text">
+                Informe o endereço onde deseja receber seu pedido
+              </Text>
+            </div>
+          </InfoSection>
+          <form>
+            <div className="wrapper">
+              <div className="row">
+                <InputContent placeholder="CEP" />
+              </div>
+              <div className="row second">
+                <InputContent placeholder="Rua" />
+              </div>
+              <div className="row third">
+                <InputContent placeholder="Número" />
+                <InputContent className="caralho" placeholder="Complemento" />
+              </div>
+              <div className="row fourth">
+                <InputContent placeholder="Bairro" />
+                <InputContent placeholder="Cidade" />
+                <InputContent placeholder="UF" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardContent>
+          <InfoSection>
+            <div>
+              <CurrencyDollar
+                size={22}
+                color={theme.palette.purple.main}
+              ></CurrencyDollar>
+            </div>
+            <div>
+              <Text size="m" color="subtitle">
+                Pagamento
+              </Text>
+              <Text size="s" color="text">
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </Text>
+            </div>
+          </InfoSection>
+          <SelectPaymentMethod>
+            <div>
+              <CreditCard size={16} color={theme.palette.purple.main} />
+              <p>CARTÃO DE CRÉDITO</p>
+            </div>
+            <div>
+              <Bank size={16} color={theme.palette.purple.main} />
+              <p>CARTÃO DE DÉBITO</p>
+            </div>
+            <div>
+              <Money size={16} color={theme.palette.purple.main} />
+              <p>DINHEIRO</p>
+            </div>
+          </SelectPaymentMethod>
+        </CardContent>
+      </div>
+      <div>
+        <Title size="xs" color="subtitle">
+          Cafés selecionados
+        </Title>
+
+        <CardContent>
+          {coffees.slice(0, 3).map((coffee) => (
+            <>
+              <CoffeeSelected key={coffee.id}>
+                <img
+                  src={`../../src/assets/coffeeCardImage/${coffee.image}`}
+                  alt=""
+                />
+                <div className="content">
+                  <Text color={'subtitle'}>{coffee.name}</Text>
+                  <div className="actions">
+                    <Counter />
+                    <DeleteButton>
+                      <Trash size={16} color={theme.palette.purple.main} />
+                      <p>REMOVER</p>
+                    </DeleteButton>
+                  </div>
+                </div>
+                <Text size={'m'} weight={'bold'}>
+                  R$ {coffee.price.toFixed(2).toString().replace('.', ',')}
+                </Text>
+              </CoffeeSelected>
+              <hr />
+            </>
+          ))}
+
+          <div className="itemsPrice">
+            <div>
+              <Text size="s">Total de itens</Text>
+              <Text size="m">R$ 19,20</Text>
+            </div>
+            <div>
+              <Text size="s">Entrega</Text>
+              <Text size="m">R$ 7,70</Text>
+            </div>
+            <div>
+              <Text size="l" weight="bold" color="subtitle">
+                Total
+              </Text>
+              <Text size="l" weight="bold" color="subtitle">
+                R$ 26,90
+              </Text>
+            </div>
+          </div>
+
+          <ConfirmOrderButton>
+            <p>CONFIRMAR PEDIDO</p>
+          </ConfirmOrderButton>
+        </CardContent>
+      </div>
+    </CheckoutComponent>
+  )
+}
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,150 @@
+import styled from 'styled-components'
+
+export const CheckoutComponent = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  justify-content: center;
+
+  margin-top: 10rem;
+  width: 100%;
+`
+
+export const CardContent = styled.div`
+  padding: 2.5rem;
+  background: ${(props) => props.theme.palette.base.card};
+  border-radius: 6px;
+  margin-top: 0.75rem;
+
+  form {
+    margin-top: 2rem;
+
+    .wrapper {
+      display: flex;
+      flex-direction: column;
+      .row {
+        margin-bottom: 1rem;
+        display: flex;
+        flex: auto;
+        flex-grow: 1;
+        flex-shrink: 1;
+        input {
+          margin-right: 0.75rem;
+        }
+      }
+
+      .second > input {
+        flex-basis: 100%;
+      }
+
+      .third > input:not(:first-child) {
+        flex: auto;
+      }
+
+      .fourth > input:not(:first-child) {
+        flex: auto;
+      }
+
+      .fourth > input:last-child {
+        width: 3.75rem;
+      }
+    }
+  }
+
+  hr {
+    height: 1px;
+    width: 100%;
+    background-color: ${(props) => props.theme.palette.base.button};
+    border: none;
+    margin: 1.5rem 0;
+  }
+
+  .itemsPrice {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+
+    div {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+`
+
+export const InfoSection = styled.div`
+  gap: 0.5rem;
+  display: flex;
+  margin-bottom: 2rem;
+`
+export const SelectPaymentMethod = styled.div`
+  display: flex;
+  gap: 0.75rem;
+
+  div {
+    display: flex;
+    gap: 0.75rem;
+    width: 11rem;
+    padding: 1rem;
+    border-radius: 6px;
+    background: ${(props) => props.theme.palette.base.button};
+    font-size: 12px;
+    color: ${(props) => props.theme.palette.base.text};
+    svg {
+      vertical-align: center;
+    }
+  }
+`
+
+export const CoffeeSelected = styled.div`
+  display: flex;
+  gap: 1.25rem;
+
+  img {
+    width: 4rem;
+    height: 4rem;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+
+    .actions {
+      display: flex;
+      gap: 0.5rem;
+      margin-top: 0.5rem;
+      margin-right: 3.125rem;
+    }
+  }
+`
+
+export const DeleteButton = styled.button`
+  display: flex;
+  padding: 0.5rem;
+  background: ${(props) => props.theme.palette.base.button};
+  border-radius: 6px;
+  border: none;
+
+  p {
+    font-size: 12px;
+    line-height: 160%;
+    color: ${(props) => props.theme.palette.base.text};
+  }
+`
+export const ConfirmOrderButton = styled.button`
+  background: ${(props) => props.theme.palette.yellow.main};
+  width: 100%;
+  height: 2.875rem;
+  border-radius: 6px;
+  border: none;
+
+  margin-top: 2rem;
+
+  p {
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 160%;
+    color: ${(props) => props.theme.palette.white};
+  }
+`
