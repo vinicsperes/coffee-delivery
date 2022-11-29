875c7a569108210f457088b2156482ecd32a3de8
ohperes <peresvini2@gmail.com>
2022-11-28 21:41:21 -0300

    Fix route name 🐞

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { Cart } from './pages/Cart'
+import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
 
 export function Router() {
@@ -8,7 +8,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   )
