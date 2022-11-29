3d991c5dee9bdd9baa187aef628599f03029e38e
ohperes <peresvini2@gmail.com>
2022-11-28 21:40:25 -0300

    Create Link to Home and Checkout pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { HeaderComponent, HeaderContainer } from './styles'
 import { DefaultTheme } from '../../styles/themes/default'
 import { useTheme } from 'styled-components'
 import { Text } from '../../styles/themes/textRule'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   const theme = useTheme() as DefaultTheme
@@ -11,8 +12,9 @@ export function Header() {
   return (
     <HeaderComponent>
       <HeaderContainer>
-        <img src={logo} alt="Logotipo Coffee Delivery" />
-
+        <NavLink to="/" title="Catálogo">
+          <img src={logo} alt="Logotipo Coffee Delivery" />
+        </NavLink>
         <div className="actions">
           <div className="location">
             <MapPin size={22} weight="fill" color={theme.palette.purple.main} />
@@ -20,21 +22,23 @@ export function Header() {
               Joinville, SC
             </Text>
           </div>
-          <div className="cart">
-            <ShoppingCart
-              size={22}
-              weight="fill"
-              color={theme.palette.yellow.dark}
-            />
-            <Text
-              className="itemCounter"
-              size="s"
-              weight="bold"
-              color={theme.palette.white}
-            >
-              3
-            </Text>
-          </div>
+          <NavLink to="/checkout" title="Checkout">
+            <div className="cart">
+              <ShoppingCart
+                size={22}
+                weight="fill"
+                color={theme.palette.yellow.dark}
+              />
+              <Text
+                className="itemCounter"
+                size="s"
+                weight="bold"
+                color={theme.palette.white}
+              >
+                3
+              </Text>
+            </div>
+          </NavLink>
         </div>
       </HeaderContainer>
     </HeaderComponent>
