663d642b68d8fb2d4b4e720899f22092a5b16b7c
ohperes <peresvini2@gmail.com>
2022-11-30 22:24:30 -0300

    Add sucess route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Checkout } from './pages/Checkout'
+import { ConfirmedOrder } from './pages/ConfirmedOrder'
 import { Home } from './pages/Home'
 
 export function Router() {
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="checkout" element={<Checkout />} />
+        <Route path="sucess" element={<ConfirmedOrder />} />
       </Route>
     </Routes>
   )
