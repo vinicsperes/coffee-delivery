6241abb6fe26acb97a44be3a7e85b658937daa64
ohperes <peresvini2@gmail.com>
2022-12-08 21:13:03 -0300

    Receive cartItems in CoffeeCard

--- src/pages/Home/components/CoffeeList/index.tsx
+++ src/pages/Home/components/CoffeeList/index.tsx
@@ -2,8 +2,12 @@ import { CoffeeCard } from '../CoffeeCard'
 import { CoffeeListComponent, CoffeeListWrapper, OurCoffees } from './styles'
 import { coffees } from '../../../../data/coffeeList'
 import { Title } from '../../../../styles/themes/textRule'
+import { CartContext } from '../../../../contexts/CartContext'
+import { useContext } from 'react'
 
 export function CoffeeList() {
+  const { cartItems } = useContext(CartContext)
+
   return (
     <CoffeeListComponent>
       <OurCoffees>
@@ -12,7 +16,7 @@ export function CoffeeList() {
         </Title>
         <CoffeeListWrapper>
           {coffees.map((coffee) => (
-            <CoffeeCard key={coffee.id} coffee={coffee} />
+            <CoffeeCard key={coffee.id} coffee={coffee} cartItems={cartItems} />
           ))}
         </CoffeeListWrapper>
       </OurCoffees>
